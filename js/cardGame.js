7397ca9b0bc1c263642264b4c5068cce73591e2c
TheRanger22 <schandingl1@nku.edu>
2019-11-23 14:40:42 -0500

    added game timer and victory alert

--- js/cardGame.js
+++ js/cardGame.js
@@ -1,9 +1,6 @@
 /*
-//TODO: Show cards for a few seconds before flipping them down face.
 //TODO: Scoring System
 //TODO: Highlight selected card
-
-
  */
 /** This is the array that contains the main deck (52)*/
 var deck=[
@@ -15,22 +12,27 @@ var deck=[
 var deck1=[];
 var deck2=[];
 /** The arrays that contain the cards that have been matched */
-var deck1matched=[];
-var deck2matched=[]; //TODO: Delete one of the 2 arrays
+var deckMatched=[];
 /** Variables that contain the value of the card picked from each deck. */
 var deck1flipped='';
 var deck2flipped='';
 /** Variables that identify the 2 divs for the decks */
 var deck1div=document.getElementById('deck1');
 var deck2div=document.getElementById('deck2');
-/***/
-var score = 0;
+/** Time keeping */
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+var timer;
+
 
 
 /**
  * Initialize a new game
  */
 function startGame(){
+    /** Start timer */
+
     /** Get the difficulty of the game (affects how many cards are displayed) */
     var element=document.getElementById('difficulty');
     var value=element.options[element.selectedIndex].value;
@@ -52,15 +54,16 @@ function startGame(){
 
     for(i=0;i<value;i++){
         /** Create the image elements */
-        card1='<div class="col-3 mb-2"><img class="card" data-card="' +deck1[i] +'" src="img/red_back.png" style="width:100%" /></div>';
-        card2='<div class="col-3 mb-2"><img class="card" data-card="' +deck2[i] +'" src="img/yellow_back.png" style="width:100%" /></div>';
+        card1='<div class="col-3 mb-2 text-center border"><img class="card" data-card="' +deck1[i] +'" src="img/red_back.png" style="width:100%" /></div>';
+        card2='<div class="col-3 mb-2 text-center border"><img class="card" data-card="' +deck2[i] +'" src="img/yellow_back.png" style="width:100%" /></div>';
         /** Add images to the 2 decks */
         deck1div.innerHTML+=card1;
         deck2div.innerHTML+=card2;
     }
 }
 function initializeBoard(){
-
+    stopTimer();
+    resetTimer();
     /** Clear the content of the divs that gold the 2 decks*/
     deck1div.innerHTML='';
     deck2div.innerHTML='';
@@ -68,11 +71,12 @@ function initializeBoard(){
     deck1=[];
     deck2=[];
     /** Clear the cards that have been matched */
-    deck1matched=[];
-    deck2matched=[];
+    deckMatched=[];
     /** Clear the variables that contain the cards that have been flipped */
     deck1flipped='';
     deck2flipped='';
+
+    startTimer();
 }
 
 /** When the user clicks on deck1 */
@@ -80,37 +84,18 @@ deck1div.addEventListener('click',function(event){
     /** If the user clicks outside a card, don't do anything */
     if(event.target.getAttribute('data-card')==undefined) return;
     /** Checks if card has been matched already */
-    if(deck1matched.includes(event.target.getAttribute('data-card'))){return;}
+    if(deckMatched.includes(event.target.getAttribute('data-card'))){return;}
     /** get the value of the card and set it to flipped */
     deck1flipped=event.target.getAttribute('data-card');
     console.log("deck1flipped - "+deck1flipped);
     console.log("deck2flipped - "+deck2flipped);
     /** Flip all the cards */
-    flipDeck('deck1',event.target.getAttribute('data-card'),'red_back');
+    flipUnmatchedCards('deck1','red_back');
     /** Flip the selected cards */
     event.target.src='img/'+event.target.getAttribute('data-card')+'.png';
 
     if(deck2flipped.length>0){
-        if(deck2flipped==deck1flipped){
-            /** We have a match */
-            /** add the cards into the matched array.*/
-
-            
-            deck1matched.push(event.target.getAttribute('data-card'));
-            deck2matched.push(event.target.getAttribute('data-card'));
-            /** Clear the flipped cards*/
-            deck1flipped='';
-            deck2flipped='';
-            console.log(deck1matched); //TODO: Delete deck2matched - Redundant
-
-        }else{
-            return; //Debug
-            /** Clear flipped cards */
-            setTimeout(function(){deck1flipped='';
-                deck2flipped='';
-                flipUnmatchedCards('deck1','red_back');
-                flipUnmatchedCards('deck2','yellow_back');},500);
-        }
+        compareCards(event.target.getAttribute('data-card'));
     }
     console.log(event.target.getAttribute('data-card'));
 });
@@ -119,54 +104,41 @@ deck2div.addEventListener('click',function(event){
     /** If the user clicks outside a card, don't do anything */
     if(event.target.getAttribute('data-card')==undefined) return;
     /** Checks if card has been matched already */
-    if(deck2matched.includes(event.target.getAttribute('data-card'))){return;}
+    if(deckMatched.includes(event.target.getAttribute('data-card'))){return;}
     /** get the value of the card and set it to flipped */
     deck2flipped=event.target.getAttribute('data-card');
-
     console.log("deck1flipped - "+deck1flipped);
     console.log("deck2flipped - "+deck2flipped);
     /** Flip all the cards */
-    flipDeck('deck2',event.target.getAttribute('data-card'),'yellow_back');
+    flipUnmatchedCards('deck2', 'yellow_back');
     /** Flip the selected cards */
     event.target.src='img/'+event.target.getAttribute('data-card')+'.png';
-
     if(deck1flipped.length>0){
-        if(deck2flipped==deck1flipped){
-            /** We have a match */
-            /** add the cards into the matched array.*/
-            deck1matched.push(event.target.getAttribute('data-card'));
-            deck2matched.push(event.target.getAttribute('data-card'));
-            /** Clear the flipped cards*/
-            deck1flipped='';
-            deck2flipped='';
-            console.log(deck1matched); //TODO: Delete deck2matched - Redundant
-        }else{
-            //return; //Debug
-            /**We don't have a match*/
-            console.log("deck2div - not a match")
-            setTimeout(function(){deck1flipped='';
-                deck2flipped='';
-                flipUnmatchedCards('deck1','red_back');
-                flipUnmatchedCards('deck2','yellow_back');},500);
-        }
+        compareCards(event.target.getAttribute('data-card'));
     }
     console.log(event.target.getAttribute('data-card'));
 });
+function compareCards(cardVal){
+    if(deck2flipped==deck1flipped){
+        /** We have a match */
+        /** add the cards into the matched array.*/
+        deckMatched.push(cardVal);
 
-function flipDeck(deck, selectedCard='',back=''){
-    var deck=document.getElementById(deck);
-    var cards = deck.getElementsByClassName('card');
-    var currentCard;
-    for(i=0;i<cards.length;i++){
-        currentCard=cards[i].getAttribute('data-card');
-        if(deck=='deck1'){
-            if(currentCard!=selectedCard && !deck1matched.includes(currentCard)) cards[i].src='img/'+back+'.png';
-        }else{
-            if(currentCard!=selectedCard && !deck2matched.includes(currentCard)) cards[i].src='img/'+back+'.png';
-        }
+        /** Clear the flipped cards*/
+        deck1flipped='';
+        deck2flipped='';
+        console.log(deckMatched);
+        checkEndgame();
+    }else{
+        //return; //Debug
+        /**We don't have a match*/
+        console.log("deck2div - not a match")
+        setTimeout(function(){deck1flipped='';
+            deck2flipped='';
+            flipUnmatchedCards('deck1','red_back');
+            flipUnmatchedCards('deck2','yellow_back');},500);
     }
 }
-
 function flipUnmatchedCards(deck,back=''){
     /**Select one of the two decks*/
     var deck=document.getElementById(deck);
@@ -179,9 +151,9 @@ function flipUnmatchedCards(deck,back=''){
         currentCard=cards[i].getAttribute('data-card');
         /** Check if the card has been matched so that we don't flip it */
         if(deck=='deck1'){
-            if(!deck1matched.includes(currentCard)) cards[i].src='img/'+back+'.png';
+            if(!deckMatched.includes(currentCard)) cards[i].src='img/'+back+'.png';
         }else{
-            if(!deck2matched.includes(currentCard)) cards[i].src='img/'+back+'.png';
+            if(!deckMatched.includes(currentCard)) cards[i].src='img/'+back+'.png';
         }
     }
 }
@@ -204,17 +176,40 @@ function shuffle(array) {
 
     return array;
 
-};
+}
 /** Check if the game is over */
 function checkEndgame(){
-    if(deck1.length==deck1matched){}
-}
+    setTimeout(function(){
+        if(deck1.length==deckMatched.length){
+            stopTimer();
+            alert('You won!')}}, 3000)}
 function grayOutMatched(){}
 //TODO: Show cards briefly at the start before flipping them down face
 //TODO: Scoring System
 //TODO: Highlight selected card
 function highlightSelected(){}
-//TODO: Add timer
-function timeKeeper(){}
+/** Timer to show how long the player has taken */
+function startTimer() {
+    timer = setInterval(countTime, 1000);
+} //Starts the timer
+function countTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+} //Keeps track of the seconds
+function stopTimer(){
+    clearInterval(timer);
+} //Stops the timer
+function resetTimer(){
+    totalSeconds = 0;
+} //Resets the value of the timer to 0
+function pad(val){
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
 //TODO: Add matches per minute
 //TODO: Gray out cards that have been matched
